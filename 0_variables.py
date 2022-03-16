04f0aaab4bf07f750ca0d0050cb25a2790dd2607
domashniy-hleb <53650114+domashniy-hleb@users.noreply.github.com>
2022-03-16 23:25:00 +0200

    Create 0_variables.py

---
+++ 0_variables.py
@@ -0,0 +1,23 @@
+a = 3
+b = 1.7
+c = "Hello"
+d = 'World!'
+e = False
+
+# This is a one line comment
+"""
+First comment line
+Second comment line
+Third comment line
+"""
+print(type(a), type(b), type(c), type(d), type(e), sep="#", end=" ")
+print(a, b, c, d, e, sep=", ")
+
+# / * + - % **
+print(2 ** 3)
+
+print(1 + 9, "Hello" + ' World!')
+print("abc" * 3)
+
+# += -= /= *=
+a += 1  # a = a + 1
